f35fdaf435d2df88bf88781abb60134c5e0dec53
Gabriele Francine <gabrielesjefrancine@hotmail.com>
2018-09-20 20:12:33 -0300

    versão inicial do meu projeto

--- aula de programação/main.py
+++ aula de programação/main.py
@@ -1,1 +1,18 @@
-print("Oi, mundo!")
+import math
+
+a=int(input("informe valor de a: "))
+b= int (input("informe valor de b: "))
+c= int(input("informe o valor de C:"))
+
+delta = b * b - 4 * a * c
+
+print("Delta: ", delta )
+
+if delta>=0
+    x1= (-b + math.sqrt(delta)) / (2 * a)
+    x2= (-b - math.sqrt((delta))/ (2 * a)
+    print("raiz x1: ", x1)
+    print("raiz x2: ", x2)
+else:
+    print("não existem raizes reais")
+          )
