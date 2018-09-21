038bfc5822da5b545a4a405db0e53ba53d1d04b5
gabriele.fsalves <gabrielesjefrancine@hotmail.com>
2018-09-20 21:53:42 -0300

    versão inicial do meu projeto

---
+++ atividade1.py
@@ -0,0 +1,22 @@
+
+n1 = int(input("digite o primeiro numero: "))
+n2 = int(input("digite o segundo numero: "))
+n3 = int(input("digite o terceiro numero: "))
+
+if n1>n2>n3:
+    print ("o maior numero e", n1, "e o maior numero e",n3)
+
+if n1>n3>n2:
+        print ("o maior numero e", n2, "e o maior numero e", n2)
+
+if n2>n1>n3:
+    print ("o maior numero e", n2, "e o menor numero e", n3)
+
+if n2>n3>n1:
+    print ("o maior numero e", n2, "e o menor numero e", n1)
+
+if n3>n2>n1:
+    print (" o maior numero e", n3, " e o menor numero e", n1)
+
+if n3>n1>n2:
+    print ("O maior numero e", n3, " e o menor numero e", n1)
