74f6a96be656735b175b41d34e3103bea415e07a
gabriele.fsalves <gabrielesjefrancine@hotmail.com>
2018-09-27 19:52:25 -0300

    versão inicial do meu projeto

--- ex3/main.py
+++ ex3/main.py
@@ -1,7 +1,6 @@
-def main()
+def main():
     m = int(input("digite o valor do metro"))
     s = int (input("digite os segundo"))
-    kms= math.sqrt( m*1*3600/1000))
-    print("metros por segundo e igual a km por hora:", kmh)
+    kmh= math.  m*1*3600/1000))
+    print(" igual a km por hora:", kmh)
 
-main()
